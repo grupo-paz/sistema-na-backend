8c7e12be7fe688f5581225d9da869f135446a652
Ryan Pavini <r236799@dac.unicamp.br>
2025-11-14 20:54:30 -0300

    fix(backend): Corrigindo detalhes pro deploy

--- backend/src/middlewares/apiKeyMiddleware.ts
+++ backend/src/middlewares/apiKeyMiddleware.ts
@@ -5,10 +5,6 @@ export default function apiKeyMiddleware(
   res: Response,
   next: NextFunction,
 ) {
-  if (req.method === 'OPTIONS') {
-    return next();
-  }
-  
   const apiKey = req.headers['x-api-key'];
   const expectedApiKey = process.env.API_KEY;
 
